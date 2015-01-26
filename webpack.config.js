51737725b9abb93e0ce3d30cc9161f44a3e7a2d9
Nukr <nukrs.w@gmail.com>
2015-01-27 00:44:20 +0800

    fix webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   module:{
     loaders: [
-      { test: /\.css$/, loader: 'css-loader'},
+      { test: /\.css$/, loader: 'style-loader!css-loader'},
       { test: /\.less$/, loader: 'style-loader!css-loader!less-loader'},
       { test: /\.jsx?$/, loaders: ['react-hot', '6to5-loader?experimental&optional=coreAliasing'], exclude: /node_modules/}
     ]

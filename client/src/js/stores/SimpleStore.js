7b1563906add91ca2202259e76b17a0828c4a732
Nukr <nukrs.w@gmail.com>
2015-01-26 10:10:55 +0800

    add some comment

--- client/src/js/stores/SimpleStore.js
+++ client/src/js/stores/SimpleStore.js
@@ -2,34 +2,24 @@ const AppDispatcher = require('../dispatcher/AppDispatcher');
 const AppConstants = require('../constants/AppConstants');
 const actions = require('../actions/AppActionCreator');
 
-var EventEmitter = require('events').EventEmitter; // 取得一個 pub/sub 廣播器
+const EventEmitter = require('events').EventEmitter; // 取得一個 pub/sub 廣播器
 
-//========================================================================
-//
-// Public API
-
-// 等同於 TodoStore extends EventEmitter
-// 從此取得廣播的能力
-// 由於將來會返還 TodoStore 出去，因此下面寫的會全變為 public methods
-var data = {
-  a: 1,
-  b: 2,
-  c: 3
-}
-
-var Store = {};
-
-// 所有 todo 資料
-
-// 目前選取的 todo 項目
+let Store = {};
 
 /**
- * 建立 Store class，並且繼承 EventEMitter 以擁有廣播功能
+ * @description
+ * Object.assign(obj1, obj2, ...) 把 Object.assign 就是把所有obj2 以後的 Object
+ * 都塞進去 obj1 這是 ES6 的 feature 現在必須使用 6to5 這個套件來處理，
+ * 我在 webpack.config.js 都有針對這一部分做處理
+ *
+ * 讓 Store 擁有所有 EventEmitter 的 method
+ * 主要是讓他能有 .on .emit 這兩個功能
  */
+
 Object.assign(Store, EventEmitter.prototype, {
 
   getTruth() {
-    return data;
+    return {};
   },
 
   addChangeListener(callback) {
@@ -42,6 +32,13 @@ Object.assign(Store, EventEmitter.prototype, {
 
 });
 
+/**
+ * 這邊是接收 Dispatcher 的地方，我們會針對 action.actionType 去 switch
+ *
+ * dispatchToken 只是一個簡單的 string，記錄著像 ID_1，ID_2 這樣的字串
+ * 可以用在 waitFor 裡面，當有兩個以上的 AppDispatcher 註冊這個事件就可以靠這個 Token
+ * 安排順序
+ */
 Store.dispatchToken = AppDispatcher.register(function eventHandlers(evt) {
 
   var action = evt.action;
@@ -56,5 +53,5 @@ Store.dispatchToken = AppDispatcher.register(function eventHandlers(evt) {
   }
 })
 
-//
+
 module.exports = Store;
